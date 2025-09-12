e84987434e478e51dbbd94de2a46878ddbc244c4
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-09-13 01:01:36 +0300

    LR1.N2 "Test flags for reading and creating window"

--- test_opencv_on_python.py
+++ test_opencv_on_python.py
@@ -1,4 +1,3 @@
-
 from random import randint
 
 import cv2
@@ -8,3 +7,28 @@ img = cv2.imread(r'image.jpg')
 cv2.imshow('output', img)
 cv2.waitKey(0)
 
+
+img = cv2.imread(r'image.jpg', cv2.IMREAD_GRAYSCALE)
+cv2.imshow('output2', img)
+cv2.waitKey(0)
+
+img = cv2.imread(r'image.jpg', cv2.IMREAD_COLOR_BGR)
+cv2.imshow('output2', img)
+cv2.waitKey(0)
+
+img = cv2.imread(r'image.jpg', cv2.IMREAD_ANYCOLOR)
+cv2.imshow('output2', img)
+cv2.waitKey(0)
+
+#Тут кроче флаги
+img = cv2.imread(r'image.jpg', cv2.WINDOW_AUTOSIZE)
+cv2.imshow('output2', img)
+cv2.waitKey(0)
+
+img = cv2.imread(r'image.jpg', cv2.WINDOW_FULLSCREEN)
+cv2.imshow('output2', img)
+cv2.waitKey(0)
+
+img = cv2.imread(r'image.jpg', cv2.WINDOW_GUI_NORMAL)
+cv2.imshow('output2', img)
+cv2.waitKey(0)
